a281f665054aaaf77b252130a38fea532d8605d8
mazer <mazer0701@gmail.com>
2024-06-13 00:20:15 +0800

    change training code from script to function

---
+++ src/train.py
@@ -0,0 +1,73 @@
+import json
+import torch
+from torch import optim
+from torch.utils.data import DataLoader
+from dataset import *
+from model import *
+
+def trainTestWithSameRankingPage(printResult = True):
+    with open('./dataset/asmrAllItemDict.json', 'r') as infile:
+        itemDict = json.load(infile)
+    with open('./dataset/asmrAllRankItem.json', 'r') as infile:
+        rankItem = json.load(infile)
+
+    rankItem, testRankItem = orderedTrainTestSplit(rankItem, 0.1)
+    postivePairsDataset, minMaxScaler = getNormalizedDataset(rankItem, itemDict)
+    dataloader = DataLoader(postivePairsDataset, batch_size = 64, shuffle = True)
+
+    model = RankNet().to(getDevice())
+    lossFunction = nn.BCEWithLogitsLoss()
+    optimizer = optim.Adam(model.parameters(), lr = 0.01, weight_decay = 0)
+    epoch = 3
+    for i in range(epoch):
+        trainCurrentRate = train(dataloader, model, lossFunction, optimizer)
+        if (printResult):
+            print(f'epoch{i} trainCurrentRate: {trainCurrentRate:.2f}') 
+
+    postivePairsDataset, _ = getNormalizedDataset(testRankItem, itemDict, minMaxScaler)
+    dataloader = DataLoader(postivePairsDataset, batch_size = 64, shuffle = False)
+    testCurrentRate = test(dataloader, model)
+    if (printResult):
+        print(f'testCurrentRate: {testCurrentRate:.2f}')
+    return trainCurrentRate, testCurrentRate
+
+def trainTestWithOtherRankingPage(learningRate = 0.001, l1Weight = 0.001, l2Weight = 0.001, shareScaler = False, printResult = True):
+    with open('./dataset/asmrAllItemDict.json', 'r') as infile:
+        itemDict = json.load(infile)
+    with open('./dataset/asmrAllRankItem.json', 'r') as infile:
+        rankItem = json.load(infile)
+
+    postivePairsDataset, minMaxScaler = getNormalizedDataset(rankItem, itemDict)
+    dataloader = DataLoader(postivePairsDataset, batch_size = 64, shuffle = True)
+
+    model = RankNet().to(getDevice())
+    lossFunction = nn.BCEWithLogitsLoss()
+    optimizer = optim.Adam(model.parameters(), lr = learningRate, weight_decay = l2Weight)
+    epoch = 3
+    for i in range(epoch):
+        trainCurrentRate = train(dataloader, model, lossFunction, optimizer, l1Weight)
+        if (printResult):
+            print(f'epoch{i} trainCurrentRate: {trainCurrentRate:.2f}')
+
+    with open('./dataset/gameAllItemDict.json', 'r') as infile:
+        itemDict = json.load(infile)
+    with open('./dataset/gameAllRankItem.json', 'r') as infile:
+        rankItem = json.load(infile)
+
+    if (shareScaler):
+        postivePairsDataset, minMaxScaler = getNormalizedDataset(rankItem, itemDict, minMaxScaler)
+    else:
+        postivePairsDataset, minMaxScaler = getNormalizedDataset(rankItem, itemDict)
+    dataloader = DataLoader(postivePairsDataset, batch_size = 64, shuffle = False)
+    testCurrentRate = test(dataloader, model)
+    if (printResult):
+        print(f'testCurrentRate: {testCurrentRate:.2f}')
+
+    return trainCurrentRate, testCurrentRate
+
+if __name__ == '__main__':
+    print('trainTestWithSameRankingPage')
+    trainTestWithSameRankingPage()
+    print('=============================')
+    print('trainTestWithOtherRankingPage')
+    trainTestWithOtherRankingPage()
